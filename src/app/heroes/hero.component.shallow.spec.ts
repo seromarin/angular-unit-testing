9e8288a2b9d6c971773447db911325bb68981c0a
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-04-17 23:21:07 -0500

    Hero component test

---
+++ src/app/heroes/hero.component.shallow.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed, ComponentFixture } from "@angular/core/testing"
+import { HeroComponent } from "../hero/hero.component"
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { By } from "@angular/platform-browser";
+
+describe('HeroComponent (shallow test)', () => {
+    let fixture: ComponentFixture<HeroComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [HeroComponent],
+            schemas: [NO_ERRORS_SCHEMA]
+        });
+        fixture = TestBed.createComponent(HeroComponent);
+    })
+
+    it('should have the correct hero', () => {
+        fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 }
+
+        expect(fixture.componentInstance.hero.name).toEqual('SuperDude')
+    })
+
+    it('should render the hero name in an anchor tag', () => {
+        fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 }
+        fixture.detectChanges();
+
+        const de = fixture.debugElement.query(By.css('a'))
+        expect(de.nativeElement.textContent).toContain('SuperDude')
+        
+        // expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude')
+    })
+
+})
